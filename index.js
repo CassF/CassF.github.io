51ab1b2c52678c0afb9d129f509c3b35f2229c72
CassF <cassandrafrimpong@gmail.com>
2016-11-24 23:23:42 +0000

    added the projects section code and some reference code

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ $("#sidebar > ul > li > a").click(function(e) {
     // Call the scroll function
     goToByScroll(this.id);           
 });
+
+jQuery(function($){
+
+	$('#contact').click( function(e){
+		$('#greeting').animate({top: '0',opacity: '0'});
+		e.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 300, function(){
+			$('#greeting').show();
+			$('#greeting').animate({ top: '-45px', opacity: '1' },300, function(){
+				$('#greeting').delay(1500).animate({ top: '-100px', opacity: '0'},300)
+			});
+		});
+	});
+
+	$('#comment-count').click( function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:($(this.hash).offset().top)-125}, 300);
+	});
+
+	$('#ct-photo').click( function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:($(this.hash).offset().top)-125}, 300);
+	});
+
+
+});
