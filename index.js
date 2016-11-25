36169b987b34df383435109791ea86ec6ec52e8a
CassF <cassandrafrimpong@gmail.com>
2016-11-25 15:53:10 +0000

    fixed skills bar loading problem added comments for guide

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-//This is the function for the skills bar. 
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
+$(function (){
+	$('.skillbar').each(function (){
+		$(this).find('.skillbar-bar').animate({
+			width: $(this).attr('data-percent')
+		}, 5000);
 	});
 });
