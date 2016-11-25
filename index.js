327ee9d157d39bd1523446312afd82156bbfe237
CassF <cassandrafrimpong@gmail.com>
2016-11-25 12:46:23 +0000

    added and customised skils bar, added column about and experience sections

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
-// This is a functions that scrolls to #{blah}link
-function goToByScroll(id){
-    // Remove "link" from the ID
-    id = id.replace("link", "");
-    // Scroll
-    $('html,body').animate({
-    scrollTop: $("#"+id).offset().top},'slow');
-}
-$("#sidebar > ul > li > a").click(function(e) { 
-    // Prevent a page reload when a link is pressed
-    e.preventDefault(); 
-    // Call the scroll function
-    goToByScroll(this.id);           
-});
 
-jQuery(function($){
-
-	$('#contact').click( function(e){
-		$('#greeting').animate({top: '0',opacity: '0'});
-		e.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 300, function(){
-			$('#greeting').show();
-			$('#greeting').animate({ top: '-45px', opacity: '1' },300, function(){
-				$('#greeting').delay(1500).animate({ top: '-100px', opacity: '0'},300)
-			});
-		});
+//This is the function for the skills bar. 
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
 	});
-
-	$('#comment-count').click( function(e){
-		e.preventDefault();
-		$('html,body').animate({scrollTop:($(this.hash).offset().top)-125}, 300);
-	});
-
-	$('#ct-photo').click( function(e){
-		e.preventDefault();
-		$('html,body').animate({scrollTop:($(this.hash).offset().top)-125}, 300);
-	});
-
-
 });
