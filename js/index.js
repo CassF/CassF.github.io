ef97dc27b5498ddf8cdaf4e712d4c0f8d8cf8015
CassF <cassandrafrimpong@gmail.com>
2016-12-01 12:09:21 +0000

    played around with timeline, addded a test timeline to the test html

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ $( document ).ready(function(){
 /* End of navbar */ 
 
 /* Skills bar */
-$(function (){
-	$('.skillbar').each(function (){
-		$(this).find('.skillbar-bar').animate({
-			width: $(this).attr('data-percent')
-		}, 5000);
-	});
-});
+// $(function (){
+// 	$('.skillbar').each(function (){
+// 		$(this).find('.skillbar-bar').animate({
+// 			width: $(this).attr('data-percent')
+// 		}, 5000);
+// 	});
+// });
 
 /* Preloader */
 let myVar;
@@ -26,5 +26,41 @@ function showPage() {
 }
 /* End of preloader */ 
 
-/* TIMELINE */
+/* TEST TIMELINE */
+$(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
 /* END OF TIMELINE */
+/* FOOTER */
+$(function(){
+    $("#flip").click(function(e){
+    e.preventDefault();
+        $("#panel").slideToggle();
+         $('html, body').animate({
+        scrollTop: 10000
+    }, 2500);
+    });
+});
