d25001b9cbfa53aeb67832eac04e4f03744a75ac
CassF <cassandrafrimpong@gmail.com>
2016-12-01 19:12:03 +0000

    added the experience section and sorted the hr break problem

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $( document ).ready(function(){
 let myVar;
 
 function myFunction() {
-    myVar = setTimeout(showPage, 3000);
+    myVar = setTimeout(showPage, 2000);
 }
 
 function showPage() {
@@ -27,40 +27,10 @@ function showPage() {
 /* End of preloader */ 
 
 /* TEST TIMELINE */
-$(document).ready(function($){
-	var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
 
-	//hide timeline blocks which are outside the viewport
-	hideBlocks(timelineBlocks, offset);
 
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame) 
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
-	});
 
-	function hideBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-		});
-	}
 
-	function showBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-		});
-	}
-});
-/* END OF TIMELINE */
-/* FOOTER */
-$(function(){
-    $("#flip").click(function(e){
-    e.preventDefault();
-        $("#panel").slideToggle();
-         $('html, body').animate({
-        scrollTop: 10000
-    }, 2500);
-    });
-});
+
+
+
